60824f788e553c7a3230155eb3111ed68e947cfd
Vagdevi Gudimella <vagdevi.gudimella@ONPASSIVE.ORG>
2022-09-14 16:31:50 +0530

    Test Toast

---
+++ ToastAppLibrary/src/main/java/com/example/toastapplibrary/ToastMessage.java
@@ -0,0 +1,13 @@
+package com.example.toastapplibrary;
+//import android.widget.Toast;
+
+
+public class ToastMessage {
+    
+        public static void s(Context c,String message){
+
+            Toast.makeText(c,message,Toast.LENGTH_SHORT).show();
+
+        }
+    }
+
