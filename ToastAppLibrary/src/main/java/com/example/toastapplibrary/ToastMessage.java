49d957f56290b6492ad26120d8615720c55d261c
Vagdevi Gudimella <vagdevi.gudimella@ONPASSIVE.ORG>
2022-09-15 16:53:50 +0530

    ResolvedErrors

--- ToastAppLibrary/src/main/java/com/example/toastapplibrary/ToastMessage.java
+++ ToastAppLibrary/src/main/java/com/example/toastapplibrary/ToastMessage.java
@@ -1,10 +1,12 @@
 package com.example.toastapplibrary;
-//import android.widget.Toast;
+import android.widget.Toast;
 
+
+import android.content.Context;
 
 public class ToastMessage {
     
-        public static void s(Context c,String message){
+        public static void s(Context c, String message){
 
             Toast.makeText(c,message,Toast.LENGTH_SHORT).show();
 
